d8039e421c258749f0546b08bae8021715f9a91f
Andres-B-Developer <faburbano@unicauca.edu.co>
2021-09-26 22:02:26 -0500

    Añado Alerts

--- mitienda/src/components/Cart/ItemCount.js
+++ mitienda/src/components/Cart/ItemCount.js
@@ -7,10 +7,10 @@ const ItemCount = ({ stock, initial }) => {
     stock = Number(stock);
     initial = Number(initial);
     //Estado:
-    const [count, setCount] = useState(Number(initial));
+    const [count, setCount] = useState(initial);
     //Lógica de botones
-    let onAdd = () => { count < stock ? setCount(count + 1) : setCount(stock); };
-    let onMinus = () => { count > initial ? setCount(count - 1) : setCount(initial); };
+    let onAdd = () => { count < stock ? setCount(count + 1) : alert(`No puedes agregar más de ${stock} productos porque no hay más stock`); };
+    let onMinus = () => { count > initial ? setCount(count - 1) : alert(`El valor mínimo es: ${initial}`); };
     //Render
     return (
         <>
