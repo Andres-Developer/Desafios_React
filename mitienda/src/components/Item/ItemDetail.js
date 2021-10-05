078e5b83e06b59dd1a05e7c0182d67dbbd49a680
Andres-B-Developer <faburbano@unicauca.edu.co>
2021-10-05 08:17:23 -0500

    Agrego boton deshabilitado que muestra stock

--- mitienda/src/components/Item/ItemDetail.js
+++ mitienda/src/components/Item/ItemDetail.js
@@ -10,9 +10,13 @@ const ItemDetail = ({ itemDetail }) => {
             <Modal.Body className="d-flex">
                 <img src={itemDetail.pictureUrl} width="60%" alt="" />
                 <div>
+                    <button type="button" className="btn btn-warning" disabled data-bs-toggle="button">
+                        Stock disponible: {itemDetail.stock}
+                    </button>
                     <h4>{"Precio: " + itemDetail.price + "$"}</h4>
                     {/* Agrego ItemCount con datos de Stock traídos por props */}
                     <ItemCount stock={itemDetail.stock} initial={1} />
+
                 </div>
             </Modal.Body>
         </>
