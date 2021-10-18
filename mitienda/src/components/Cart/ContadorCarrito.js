8c8c92e0b883a89d245ce65a2f31d5ae69245cb3
Andres-B-Developer <faburbano@unicauca.edu.co>
2021-10-18 12:08:02 -0500

    Cambio lógica de contador basada en Cart Context

--- mitienda/src/components/Cart/ContadorCarrito.js
+++ mitienda/src/components/Cart/ContadorCarrito.js
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react';
 import { CartContext } from 'context/CartContext';
-import "./../../assets/css/ContadorCarrito.css"
+import "./../../assets/css/ContadorCarrito.css";
 
 
-const ContadorCarrito = ({cantidad}) => {
-    const [state, setState] = useContext(CartContext);
-    return (        
-            <span className="ContadorCarrito position-absolute badge rounded-pill bg-danger">
-                {state.count}
-            </span>
-        
-    )
-}
+const ContadorCarrito = ({ cantidad }) => {
+    //Contexto de Cart
+    const { totalItems } = useContext(CartContext);
+    return (
+        <span className="ContadorCarrito position-absolute badge rounded-pill bg-danger">
+            {totalItems != 0 ? totalItems : ''}
+        </span>
 
-export default ContadorCarrito
+    );
+};
+
+export default ContadorCarrito;
