1def2e599a0d311eebe06e3f5c1c0a70bb2ccaf7
Andres-B-Developer <faburbano@unicauca.edu.co>
2021-10-27 18:04:57 -0500

    Add estilos ItemDetail

--- src/components/Item/ItemDetail.js
+++ src/components/Item/ItemDetail.js
@@ -1,6 +1,6 @@
 import ItemCount from "./ItemCount";
 import { useState, useEffect } from 'react';
-
+import './../../assets/css/ItemDetail.css'
 
 const ItemDetail = ({ itemDetail }) => {
     const [totalProductos, setTotalProductos] = useState(0);
@@ -15,21 +15,30 @@ const ItemDetail = ({ itemDetail }) => {
         //console.log("Productos traídos de ItemCount: ", totalProductos);
     }, [totalProductos]);
     return (
-        <>
-            <h3>{itemDetail.title + " " + itemDetail.marca.toUpperCase()}</h3>
-            <div className="d-flex align-items-center">
-                <img src={`./../` + itemDetail.pictureUrl} width="30%" alt="" />
-                <div className="ms-5">
-                    <button type="button" className="btn btn-warning" disabled data-bs-toggle="button">
-                        Stock disponible: {itemDetail.stock}
-                    </button>
-                    <h4>{"Precio: " + itemDetail.price + "$"}</h4>
-                    {/* Agrego ItemCount con datos de Stock traídos por props */}
-                    {/* Le paso el evento para que desde ItemCount Me guarde la cantiidad de productos Agregados al Carrito */}
-                    <ItemCount productosAgregadosCarrito={productosAgregadosCarrito} idProducto={itemDetail.id} stock={itemDetail.stock} precio={itemDetail.price} initial={1} />
+        <div className="container d-flex justify-content-center align-items-center mt-5 pt-4">
+            <div id="columnas" className="row">
+                <div className="col-12 col-md-6">
+                    <img className ="img-fluid" src={`./../` + itemDetail.pictureUrl}  alt="" />
+                </div>
+                <div className="col-12 col-md-6 pt-3">
+                    <div className="h3 text-capitalize">{itemDetail.title + " " + itemDetail.marca.toUpperCase()}</div>
+                    <div className="h5 text-black-50 ">{itemDetail.descripcion}
+                        
+                    </div>
+                    <div className="">
+                        <div className="mt-4">
+                            <button type="button" className="btn btn-warning" disabled data-bs-toggle="button">
+                                Stock disponible: {itemDetail.stock}
+                            </button>
+                            <div className="h4 mt-3">{"Precio: " + itemDetail.price + "$"}</div>
+                            {/* Agrego ItemCount con datos de Stock traídos por props */}
+                            {/* Le paso el evento para que desde ItemCount Me guarde la cantiidad de productos Agregados al Carrito */}
+                            <ItemCount productosAgregadosCarrito={productosAgregadosCarrito} idProducto={itemDetail.id} stock={itemDetail.stock} precio={itemDetail.price} initial={1} />
+                        </div>
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
