5c761b2708cd89bbdba72488956f61f5e123cb20
Andres-Developer <faburbano2@misena.edu.co>
2021-10-30 23:18:30 -0500

    Usando Navbar de React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ProviderCart from './context/CartContext';
 import ItemListContainer from './components/Item/ItemListContainerFirebase';
-import NavBar from './components/NavBar/NavBar';
 import ItemDetailContainer from './components/Item/ItemDetailContainerFirebase';
 import Cart from "./components/Cart/CartFirebase";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/App.css';
 import { Form } from './components/Form/Form';
+import NavBootStrap from 'components/NavBar/NavBootStrap';
 
 
 function App() {
   return (
     <ProviderCart>
       <Router>
-        <NavBar />
+        <NavBootStrap/>
         <Switch>
           <Route exact path="/">
             <ItemListContainer />
--- src/components/NavBar/NavBootStrap.js
+++ src/components/NavBar/NavBootStrap.js
@@ -1,31 +1,25 @@
 import React from 'react';
-import {Navbar,NavDropdown, Nav, Container} from 'react-bootstrap';
-
+import { Navbar, NavDropdown, Nav, Container } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
+import shoppingLogo from './../../img/shoppingLogo.svg';
 
 const NavBootStrap = () => {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand><NavLink className="brand " to="/">
+                        <input type="image" alt="logo" src={shoppingLogo} height="25px" />
+                        <span>Mi TiendAPP</span>
+                    </NavLink></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown title="Categorías" id="collasible-nav-dropdown">
+                                <NavDropdown.Item > <NavLink className="nav_link" to="/category/1">Accesorios</NavLink></NavDropdown.Item>
+                                <NavDropdown.Item ><NavLink className="nav_link" to="/category/2">Zapatos</NavLink></NavDropdown.Item>
+                                <NavDropdown.Item ><NavLink className="nav_link" to="/category/3">Ropa</NavLink></NavDropdown.Item>
                             </NavDropdown>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
-                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
